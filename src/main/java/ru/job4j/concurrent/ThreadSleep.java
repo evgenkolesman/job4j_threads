780c58fda1f3d17f59ce8351ec43817561f5608f
evgenkolesman <glavstroi_e@mail.ru>
2021-08-09 00:11:51 +0300

    1.2. Режим ожидания. [#231217]

--- src/main/java/ru/job4j/concurrent/ThreadSleep.java
+++ src/main/java/ru/job4j/concurrent/ThreadSleep.java
@@ -1,5 +1,12 @@
 package ru.job4j.concurrent;
 
+/**
+ * модулируем простую загрузку любой программы
+ * вывод процентов в одном месте в консоли
+ *
+ * @author Kolesnikov Evgeniy
+ * version 1.0
+ */
 public class ThreadSleep {
     public static void main(String[] args) {
         Thread thread = new Thread(() -> {
@@ -7,14 +14,12 @@ public class ThreadSleep {
                 System.out.println("Start loading ... ");
                 for (int i = 1; i < 101; i++) {
                     Thread.sleep(100);
-                    System.out.printf("Loaded %d%s...\n", i, "%");
+                    System.out.printf("Loaded %d%s...\r", i, "%");
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
-
         thread.start();
-        System.out.println("Main");
     }
 }
