570a410c9ced84292ca3cc1f6c5dc183ff518f7b
evgenkolesman <glavstroi_e@mail.ru>
2021-08-08 13:38:52 +0300

    1. Запуск нити. Thread#start() [#1016]

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -1,11 +1,23 @@
 package ru.job4j.concurrent;
 
+/**
+ * Работа по инициализации нити,
+ * наглядное тестирование работы нескольких нитей
+ *
+ * @author Kolesnikov Evgeniy
+ * @version  1.0
+ */
 public class ConcurrentOutput {
     public static void main(String[] args) {
         Thread another = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
         another.start();
+        System.out.println(Thread.currentThread().getName());
+
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName()));
+        second.start();
         System.out.println(Thread.currentThread().getName());
     }
 }
