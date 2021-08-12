0bbbbcdd20dd2de125eb56a07a1ebeceecdb87ce
evgenkolesman <glavstroi_e@mail.ru>
2021-08-12 22:59:46 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/Flag.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+// Код для примера модели памяти
+
+public class Flag {
+    private volatile static boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(
+                () -> {
+                    while (flag) {
+                        System.out.println(Thread.currentThread().getName());
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        thread.start();
+        Thread.sleep(1000);
+        flag = false;
+        thread.join();
+    }
+}
