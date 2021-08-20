ce5ef253e8ffb4410589fbd73b11fda521617724
evgenkolesman <glavstroi_e@mail.ru>
2021-08-21 01:17:23 +0300

    Тесты на CAS

--- src/main/java/ru/job4j/atomaric/CASCount.java
+++ src/main/java/ru/job4j/atomaric/CASCount.java
@@ -6,13 +6,14 @@ import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount {
-    private final AtomicReference<Integer> count = new AtomicReference<>();
+    private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
     public void increment() {
         int a;
         do {
             a = count.get();
-        } while (a != count.compareAndExchange(a, a + 1));
+        }
+        while (!count.compareAndSet(a, a + 1));
     }
 
     public int getVal() {
--- src/test/java/ru/job4j/atomaric/CASCountTest.java
+++ src/test/java/ru/job4j/atomaric/CASCountTest.java
@@ -3,24 +3,32 @@ package ru.job4j.atomaric;
 
 import org.junit.Test;
 
+import java.util.stream.IntStream;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CASCountTest {
 
     @Test
-    public void incrementTest() {
-        CASCount casCount = new CASCount();
-        Thread first = new Thread( () -> {
-            casCount.increment();
-        });
-        first.start();
-        try {
-            first.wait(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void incrementTest() throws InterruptedException {
 
-        assertThat(casCount.getVal(), is(1));
+        CASCount casCount = new CASCount();
+        Thread first = new Thread( () ->
+            IntStream.range(1, 8).forEach(a -> casCount.increment()));
+        Thread second = new Thread( () ->
+                IntStream.range(0,10).forEach(a -> casCount.increment()));
+        Thread third = new Thread( () ->
+                IntStream.range(1,10).forEach(a -> casCount.increment()));
+
+        first.start();
+        second.start();
+        third.start();
+
+        first.join();
+        second.join();
+        third.join();
+
+        assertThat(casCount.getVal(), is(26));
     }
 }
