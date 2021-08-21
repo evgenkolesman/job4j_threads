35c5165e53360da809ec2b8e353c2facec0b4307
evgenkolesman <glavstroi_e@mail.ru>
2021-08-21 18:09:15 +0300

    3. ForkJoinPool [#315067]

---
+++ src/main/java/ru/job4j/pools/RunPool.java
@@ -0,0 +1,32 @@
+package ru.job4j.pools;
+
+import java.util.Arrays;
+
+public class RunPool {
+    public static void main(String[] args) {
+        int [] arr ={2, 1, 3, 4, 5, 6, 7, 9, 8};
+        int [] arr1 ={1, 3, 2, 4, 5, 6, 7, 8, 9, 10, 12, 11};
+
+        Arrays.stream(choseArrayAndSort(arr)).forEach(System.out :: print);
+        System.out.println("");;
+        Arrays.stream(choseArrayAndSort(arr1)).forEach(System.out :: print);
+    }
+
+   public static int[] choseArrayAndSort(int [] arr) {
+        int [] res;
+        if(arr.length <= 10) {
+            MergeSort mergeSort = new MergeSort();
+            res = mergeSort.sort(arr);
+        }
+        else {
+            ParallelMergeSort parallelMergeSort = new ParallelMergeSort(arr, arr[0], arr[arr.length -1]);
+            res = parallelMergeSort.sort(arr);
+        }
+        return res;
+   }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
