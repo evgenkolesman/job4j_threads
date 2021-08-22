5fb724aaabed4fffd5f72fbca290deb7f8705bd2
evgenkolesman <glavstroi_e@mail.ru>
2021-08-23 00:28:27 +0300

    3. ForkJoinPool [#315067 #192685]

--- src/main/java/ru/job4j/pools/FindValueInArray.java
+++ src/main/java/ru/job4j/pools/FindValueInArray.java
@@ -1,33 +1,50 @@
 package ru.job4j.pools;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
 
-public class FindValueInArray extends RecursiveTask<int[]> {
-    private final int[] arr;
+public class FindValueInArray extends RecursiveTask<Integer> {
+
+    private final int arr[];
     private final int value;
+    private final int to;
+    private final int from;
 
-    public FindValueInArray(int[] arr, int value) {
+    public FindValueInArray(int[] arr, int value, int to, int from) {
         this.arr = arr;
         this.value = value;
+        this.to = to;
+        this.from = from;
     }
 
-
-
-    @Override
-    protected int[] compute() {
-        int[] res = new int[1];
-        FindValueInArray valueInArray = new FindValueInArray(arr, value);
-
+    static int findMethod(int[] arr, int value) {
+//      AtomicInteger res = new AtomicInteger();
+        var res = -1;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                res = i;
+            }
+        }
         return res;
     }
 
-    public int findMethod(int value) {
-        AtomicInteger res = new AtomicInteger();
-        IntStream.range(0, arr.length).filter(i -> arr[i] == value).forEach(i -> res.set(i));
-        return res.get();
+    @Override
+    protected Integer compute() {
+        if(arr.length <= 10) {
+            return findMethod(arr, value);
+        }
+        int mid = (arr.length-1)/2;
+        FindValueInArray findValueInArray1 = new FindValueInArray(arr, value, to, mid);
+        FindValueInArray findValueInArray2 = new FindValueInArray(arr, value, mid +1, from);
+        findValueInArray1.fork();
+        findValueInArray2.fork();
+        return Math.max(findValueInArray1.join(), findValueInArray2.join());
     }
 
-
+    public static Integer find(int[] array, int value) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return forkJoinPool.invoke(new FindValueInArray(array, value, 0, array.length - 1));
+    }
 }
+
+
---
+++ src/test/java/ru/job4j/pools/FindValueInArrayTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.pools;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FindValueInArrayTest  {
+
+    @Test
+    public void whenLengthLessTen() {
+        int[] arr = {1,2,3};
+        int val = 3;
+
+        assertThat(FindValueInArray.find(arr, val), is(2));
+    }
+
+    @Test
+    public void whenLengthMoreTen() {
+        int [] arr = new int[12];
+        for (int i = 0; i <12; i++) {
+            arr[i] = i;
+        }
+        int val = 3;
+        assertThat(FindValueInArray.find(arr, val), is(2));
+    }
+}
